f4545d6aec321ee85cb46856eab0e36e7b75135c
gilangsuryalaga <gilangsuryalaga@gmail.com>
2019-09-13 09:45:13 +0700

    tul

---
+++ app/src/main/java/com/example/weddingseason2/Splashscreen.java
@@ -0,0 +1,23 @@
+package com.example.weddingseason2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splashscreen extends AppCompatActivity {
+    private int waktu_loading=3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent home = new Intent(Splashscreen.this, MainActivity.class);
+                startActivity(home);
+            }
+        },waktu_loading);
+    }
+}
